19a2238c1717a146c867919a4c1151f0efdb5eb2
instatechhub <instahub902@gmail.com>
2025-06-27 16:09:39 +0530

    new new

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -6,14 +6,20 @@ const Dashboard = () => {
   const { user } = useAuthStore();
   return (
     <div className="container">
-      <div className="d-flex justify-content-between">
+      <div className="d-flex justify-content-between responsive-heading">
         <h2 className="mt-3 responsive-heading">
-          Welcome,
-          {typeof user?.name === "string" ? user.name.toUpperCase() : "NA"}
+          <span style={{ fontSize: "20px" }}>Welcome Back!,</span>
+          <br />
+          <span style={{ fontSize: "20px" }}>
+            {typeof user?.name === "string" ? user.name.toUpperCase() : "NA"}
+          </span>
         </h2>
-        <h2 className="mt-3 responsive-heading" >
-          Process: 
-          {user?.processName?.toUpperCase()}
+        <h2 className="mt-3 responsive-heading">
+          <span style={{ fontSize: "20px" }}>Process:</span>
+          <span style={{ fontSize: "20px" }}>
+            {user?.processName?.toUpperCase()}
+          </span>
+          <br />
         </h2>
       </div>
       <DashboardStats />
